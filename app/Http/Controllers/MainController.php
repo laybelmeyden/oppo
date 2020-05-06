5356bfbb081bc0f4558e688ff40f1b5b68db9203
laybelmeyden <trycollens@gmail.com>
2020-05-06 17:15:17 +0300

    dasd

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -70,11 +70,15 @@ class MainController extends Controller
             'model' => request('model'),
             'shop' => request('shop'),
             'date' => request('date'),
-            'app' => request('app'),
+            'app' => request->file,
         );
         Mail::send('email.mailmain', $data, function ($message) use ($data, $to_email, $to_name) {
-            $message->from($to_email, $data['name_fill'], $data['years'], $data['city'], $data['numb'], $data['email'], $data['model'], $data['shop'], $data['date'], $data['radio'], $data['app']);
+            $message->from($to_email, $data['name_fill'], $data['years'], $data['city'], $data['numb'], $data['email'], $data['model'], $data['shop'], $data['date'], $data['radio']);
             $message->to($to_email)->subject('Message from site');
+            $message->attach($data['app']->getRealPath(), array(
+                'as' => 'app.' . $data['app']->getClientOriginalExtension(),
+                'mime' => $data['app']->getMimeType())
+            );
         });
 
         back()->with('message_1', 'После обработки вашей анкеты, мы свяжемся с вами.');
